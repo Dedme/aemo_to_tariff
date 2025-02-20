21387cd70a1be09db5fcc21bc22391a0f78e2959
Ian Connor <iconnor@projectlounge.com>
2025-02-20 14:53:40 +1000

    Add another tests for SAPN

--- test/test_sapower.py
+++ test/test_sapower.py
@@ -12,3 +12,12 @@ class TestSAPower(unittest.TestCase):
         price = sapower.convert(interval_time, tariff_code, rrp)
         loss_factor = expected_price / price
         self.assertAlmostEqual(price * 1.1678, expected_price, places=2)
+
+    def test_later_day(self):
+        interval_time = datetime(2025, 2, 20, 15, 10, tzinfo=ZoneInfo('Australia/Adelaide'))
+        tariff_code = 'RTOU'
+        rrp = -76.53
+        expected_price = 13.03587882
+        price = sapower.convert(interval_time, tariff_code, rrp)
+        loss_factor = expected_price / price
+        self.assertAlmostEqual(price * 1.1678, expected_price, places=2)
