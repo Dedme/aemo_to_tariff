2c14724ba2894f8da12c2337db4e6a944f3484a5
Ian Connor <iconnor@projectlounge.com>
2025-05-13 11:59:27 +1000

    Add more tests

--- test/test_essential.py
+++ test/test_essential.py
@@ -11,4 +11,44 @@ class TestEssentualPower(unittest.TestCase):
         expected_price = 3.858878319999999
         price = essential.convert(interval_time, tariff_code, rrp)
         loss_factor = expected_price / price
-        self.assertAlmostEqual(price * 1.1678, expected_price, places=1)
+        self.assertAlmostEqual(price * 1.168, expected_price, places=1)
+
+    def test_seven_pm(self):
+        # 2025-05-12 19:25:00+10
+        interval_time = datetime(2025, 5, 12, 19, 25, tzinfo=ZoneInfo('Australia/Sydney'))
+        tariff_code = 'BLND1AR'
+        rrp = 135.36
+        expected_price = 37.60955297
+        price = essential.convert(interval_time, tariff_code, rrp)
+        loss_factor = expected_price / price
+        self.assertAlmostEqual(price * 1.68, expected_price, places=1)
+
+    def test_seven_am(self):
+        # 2025-05-12 07:25:00+10
+        interval_time = datetime(2025, 5, 12, 7, 25, tzinfo=ZoneInfo('Australia/Sydney'))
+        tariff_code = 'BLND1AR'
+        rrp = 153.88
+        expected_price = 34.06917696
+        price = essential.convert(interval_time, tariff_code, rrp)
+        loss_factor = expected_price / price
+        self.assertAlmostEqual(price * 1.6, expected_price, places=1)
+
+    def test_four_am(self):
+        # 2025-05-10 04:15:00+10
+        interval_time = datetime(2025, 5, 20, 4, 15, tzinfo=ZoneInfo('Australia/Sydney'))
+        tariff_code = 'BLND1AR'
+        rrp = 67.73
+        expected_price = 15.23709699
+        price = essential.convert(interval_time, tariff_code, rrp)
+        loss_factor = expected_price / price
+        self.assertAlmostEqual(price * 1.48, expected_price, places=1)
+
+    def test_later_day(self):
+        # 2025-05-10 12:25:00+10
+        interval_time = datetime(2025, 5, 20, 12, 25, tzinfo=ZoneInfo('Australia/Sydney'))
+        tariff_code = 'BLND1AR'
+        rrp = -16.13
+        expected_price = 6.21313241
+        price = essential.convert(interval_time, tariff_code, rrp)
+        loss_factor = expected_price / price
+        self.assertAlmostEqual(price * 1.447, expected_price, places=1)
